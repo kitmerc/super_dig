dfe32c5b2b8e2c4937c03a17fe2ac25e22f6de60
kitmerc <97457279+kitmerc@users.noreply.github.com>
2022-08-08 23:30:29 +0300

    Update sd.sh

--- sd.sh
+++ sd.sh
@@ -1,11 +1,30 @@
-#!/usr/bin/env sh
-if [ -n "$1" ]; then continue; else echo "can't find 1st arg"; exit; fi
-DIG="dig +short"
-GREEN="$(tput setaf 2)"
-CLEAR="$(tput sgr0)"
-IPCHK="yii ip/is-our-ip"
-echo "main:" ${GREEN} "\n$($DIG A "$(hm "$1")")" ${CLEAR}
-echo "\nwww:" ${GREEN}"\n$($DIG A "$(hm "$(echo "www."$1)")")" ${CLEAR}
-echo "\nns:" ${GREEN}"\n$($DIG NS "$1")"  ${CLEAR}
-echo "\nmx:" ${GREEN}"\n$($DIG MX "$1")" ${CLEAR}
-echo "\ntxt:" ${GREEN}"\n$($DIG TXT "$1")"  ${CLEAR}
+#!/bin/bash
+if [ -z "$1" ]; then echo "can't find 1st arg"; exit; fi
+arg1=$1
+dig="dig +short"
+green="$(tput setaf 5)"
+cleare="$(tput sgr0)"
+
+onlydomain () {
+checkhttp=$(echo $arg1 | tr A-Z a-z | grep -o "http://")
+checkhttps=$(echo $arg1 | tr A-Z a-z | grep -o "https://")
+if [[ "$checkhttp" = "http://" ]];then
+                total=$(echo $arg1 | tr A-Z a-z | cut -c 8-)
+elif [[ "$checkhttps" = "https://" ]];then
+        total=$(echo $arg1 | tr A-Z a-z | cut -c 9-)
+else    
+        total=$(echo $arg1 | tr A-Z a-z)
+fi
+check=$(expr index "$total" "/")
+if [[ $check == 0 ]];then
+        echo $total
+else    
+        let "last=$check - 1" 
+        echo "$total" | cut -c -"$last"
+fi
+}
+echo -e "main:"${green}"\n$($dig A "$(onlydomain)")"${cleare}
+echo -e "\nwww:"${green}"\n$($dig A "www.""$(onlydomain)")"${cleare}
+echo -e "\nns:"${green}"\n$($dig NS "$(onlydomain)")"${cleare}
+echo -e "\nmx:"${green}"\n$($dig MX "$(onlydomain)")"${cleare}
+echo -e "\ntxt:"${green}"\n$($dig TXT "$(onlydomain)")"${cleare}
