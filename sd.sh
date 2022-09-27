afaa9c8f49a2d8e7efe7d0e5f261757834c48125
kitmerc <97457279+kitmerc@users.noreply.github.com>
2022-09-27 12:41:39 +0300

    Update sd.sh

--- sd.sh
+++ sd.sh
@@ -2,6 +2,7 @@
 if [ -z "$1" ]; then echo "can't find 1st arg"; exit; fi
 if [ -n "$2" ]; then dig="dig +short "$2""; else dig="dig +short"; fi
 arg1=$1
+yellow="$(tput setaf 3)"
 green="$(tput setaf 2)"
 cleare="$(tput sgr0)"
 ipcheck="yii ip/is-our-ip"
@@ -12,19 +13,19 @@ if [[ "$checkhttp" = "http://" ]];then
                 total=$(echo $arg1 | tr A-Z a-z | cut -c 8-)
 elif [[ "$checkhttps" = "https://" ]];then
         total=$(echo $arg1 | tr A-Z a-z | cut -c 9-)
-else    
+else
         total=$(echo $arg1 | tr A-Z a-z)
 fi
 check=$(expr index "$total" "/")
 if [[ $check == 0 ]];then
         echo $total
-else    
-        let "last=$check - 1" 
+else
+        let "last=$check - 1"
         echo "$total" | cut -c -"$last"
 fi
 }
-echo -e "main:"${green}"\n$($dig A "$(onlydomain)")"${cleare}
-echo -e "\nwww:"${green}"\n$($dig A "www.""$(onlydomain)")"${cleare}
-echo -e "\nns:"${green}"\n$($dig NS "$(onlydomain)")"${cleare}
-echo -e "\nmx:"${green}"\n$($dig MX "$(onlydomain)")"${cleare}
-echo -e "\ntxt:"${green}"\n$($dig TXT "$(onlydomain)")"${cleare}
+echo -e ${yellow}"main:"${cleare}${green}"\n$($dig A "$(onlydomain)")"${cleare}
+echo -e ${yellow}"www:"${cleare}${green}"\n$($dig A "www.""$(onlydomain)")"${cleare}
+echo -e ${yellow}"ns:"${cleare}${green}"\n$($dig NS "$(onlydomain)")"${cleare}
+echo -e ${yellow}"mx:"${cleare}${green}"\n$($dig MX "$(onlydomain)")"${cleare}
+echo -e ${yellow}"txt:"${cleare}${green}"\n$($dig TXT "$(onlydomain)")"${cleare}
